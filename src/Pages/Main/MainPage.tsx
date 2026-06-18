7bae92e3503f395cb5c2e9c70bc5c0660cf73756
ArtemDR <artem21drugov@gmail.com>
2026-06-18 22:32:30 +0300

    18.06

--- src/Pages/Main/MainPage.tsx
+++ src/Pages/Main/MainPage.tsx
@@ -1,6 +1,39 @@
+import TH_Buble from '../../assets/Tommy_Bubble.jpg'
+import CK_Buble from '../../assets/CK_Bubble.png'
+
 function WelcomePage (){
     return(
-        <h1></h1>
+        // ооболочка 
+        <div className='bg-slate-500'>
+             {/* аэрографика */}
+            <div>
+                 {/* основные блоки */}
+                <div className="flex items-center justify-center gap-[20px] mt-[225px] mb-[225px]  ">
+                    {/* левый блок */}
+                    <div className="w-[470px] h-[420px] ">
+                        <img
+                        src={TH_Buble}
+                        alt="TommyBuble"
+                        className=""
+                        />
+                        <span className="text-center text-white">
+                            Tommy Hilfiger
+                        </span>
+                    </div>
+                    {/* правый блок */}
+                    <div className="w-[470px] h-[420px] ">
+                        <img
+                        src={CK_Buble}
+                        alt="CalvinBuble"
+                        className=""
+                        />
+                        <span className="text-center text-white">
+                            Calvin klein
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import WelcomePage from './Pages/Main/MainPage.tsx'
+import MainPage from './Pages/Main/MainPage.tsx'
 import LogOut from './Pages/LogOut.tsx'
 import Header from './Components/Header.tsx'
 import Footer from './Components/Footer.tsx'
@@ -17,7 +17,7 @@ import Footer from './Components/Footer.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header/>
-    <WelcomePage/>
+    <MainPage/>
     <Footer/>
     <App />
   </StrictMode>,
